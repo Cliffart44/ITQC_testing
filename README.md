52a02e1f71cc27acb1d35bc61682e698895c8ba4
Artjom <ArtjomMashkov-job@yahoo.com>
2021-05-21 16:54:56 +0300

    Java CI with Gradle added

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Java CI with Gradle](https://github.com/Cliffart44/ITQC_testing/actions/workflows/gradle.yml/badge.svg)](https://github.com/Cliffart44/ITQC_testing/actions/workflows/gradle.yml)
--- src/test/java/PageUiTestTest.java
+++ src/test/java/PageUiTestTest.java
@@ -9,6 +9,9 @@ import static com.codeborne.selenide.Selenide.open;
 class PageUiTestTest {
     private final SelenideElement russianSlogan = $(".as-h1>.rus");
     private final SelenideElement englishSlogan = $(".as-h1 noindex");
+    private final SelenideElement switchToRussian = $(".en>.rus");
+    private final SelenideElement switchToEnglish = $(".rus>.en");
+
 
     @BeforeEach
     void setUp() {
@@ -17,12 +20,13 @@ class PageUiTestTest {
 
     @Test
     void shouldCheckSloganLiteracyRussian() {
+        if (switchToRussian.isDisplayed()) switchToRussian.click();
         russianSlogan.shouldBe(visible).shouldHave(exactOwnText("Не платите за ошибки — платите за качество вашей системы"));
     }
 
     @Test
     void shouldCheckSloganLiteracyEnglish() {
-        $(".rus>.en").click();
+        if (switchToEnglish.isDisplayed()) switchToEnglish.click();
         englishSlogan.shouldBe(visible).shouldHave(exactOwnText("Pay for the quality of your system — not for errors!"));
     }
 }
